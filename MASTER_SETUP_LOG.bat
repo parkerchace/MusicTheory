fd8a34f243ea5340399fe91e30346c412e91f8b3
Parker Chace <parkerc@american.edu>
2025-12-04 10:26:20 -0500

    chore: remove unused scripts, demos, process docs, and dev tools for public release

--- MASTER_SETUP_LOG.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Deprecated shim - use MASTER_SETUP.bat directly. This wrapper forwards to the canonical script.
-echo NOTE: This wrapper is deprecated. Forwarding to MASTER_SETUP.bat...
-call MASTER_SETUP.bat %*
--- start_midi_server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Deprecated shim - use START_SERVER.bat instead
-echo NOTE: This wrapper is deprecated. Forwarding to START_SERVER.bat...
-call START_SERVER.bat %*
